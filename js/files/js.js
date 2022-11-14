a8b485d0b066c12ddbf8dfa463761e484f29f924
doston@olcha.uz <doston@olcha.uz>
2022-11-14 10:49:10 +0500

    update styles

--- js/files/js.js
+++ js/files/js.js
@@ -11,40 +11,79 @@ document.documentElement.addEventListener("click", function (e) {
 	}
 });
 
-$('.checkbox__block').click(function() {
-	$('.checkbox__block._active').removeClass('_active').find('.radio__input').prop("checked", false);
-	$(this).addClass('_active').find('.radio__input').prop("checked", true);
-	let newImg = $(this).find('.checkbox__block-image img').attr('src');
-	$('.bg-content._slide2 img').fadeOut(300);
-	setTimeout(function () {
-		$('.bg-content._slide2 img').attr('src', newImg).fadeIn(300);
-	}, 300)
+// Выбор страны
+jQuery('.btn-links .btn').click(function () {
+	let parent = jQuery(this).closest('.btn-links');
+	parent.find('.btn._active').removeClass('_active');
+	jQuery(this).addClass('_active');
+});
+
+
+
+// Выбор турпакета и смена слайда
+jQuery('.checkbox__block').click(function() {
+	let parent = jQuery(this).closest('.section');
+	let newImg = jQuery(this).find('.checkbox__block-image img').attr('src');
+	jQuery('.checkbox__block._active').removeClass('_active').find('.radio__input').prop("checked", false);
+	jQuery(this).addClass('_active').find('.radio__input').prop("checked", true);
+	if(jQuery('body').innerWidth() >= 1200) {
+		let slide = parent.find('.bg-content img')
+		slide.fadeOut(300);
+		setTimeout(function () {
+			slide.attr('src', newImg).fadeIn(300);
+		}, 300)
+	}
+	else {
+		parent.css('background-image', 'url(' + newImg + ')');
+	}
+});
+
+// Выбор других блоков и смена слайда
+jQuery('.tourList__item').click(function() {
+	let parent = jQuery(this).closest('.section');
+	let newImg = jQuery(this).attr('data-img');
+	parent.find('.tourList__item._active').removeClass('_active').find('.radio__input').prop("checked", false);
+	jQuery(this).addClass('_active').find('.radio__input').prop("checked", true);
+	if(jQuery('body').innerWidth() >= 1200) {
+		let slide = parent.find('.bg-content img');
+		slide.fadeOut(300);
+		setTimeout(function () {
+			slide.attr('src', newImg).fadeIn(300);
+		}, 300)
+	}
+	else {
+		parent.css('background-image', 'url(' + newImg + ')');
+	}
+});
+
+// Выбор способа оплаты
+jQuery('.payment__btn').click(function () {
+	jQuery('.payment__btn').removeClass('_active');
+	jQuery(this).addClass('_active');
 })
 
-
+// Навигация при клиrе на меню сбоку и кнопки назад/далее
 jQuery(document).ready(function ($) {
-	//initialise Stellar.js
+	let section = $('.slide');
+	section.each(function () {
+		let sectionImg = $(this).find('.bg-content img').attr('src');
+		$(this).css('background-image', 'url(' + sectionImg + ')');
+	})
+	
 	$(window).stellar();
 	
-	//Cache some variables
-	var links = $('.navigation').find('li');
-	slide = $('.slide');
-	button = $('.btn');
-	mywindow = $(window);
-	htmlbody = $('html,body');
+	let links = $('.navigation').find('li');
+	let slide = $('.slide');
+	let button = $('.btn');
+	let mywindow = $(window);
+	let htmlbody = $('html,body');
 	
-	//Setup waypoints plugin
 	slide.waypoint(function (event, direction) {
-		//cache the variable of the data-slide attribute associated with each slide
-		dataslide = $(this).attr('data-slide');
-		//If the user scrolls up change the navigation link that has the same data-slide attribute as the slide to active and
-		//remove the active class from the previous navigation link
+		let dataslide = $(this).attr('data-slide');
 		
 		if (direction === 'down') {
 			$('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
 		}
-			// else If the user scrolls down change the navigation link that has the same data-slide attribute as the slide to active and
-		//remove the active class from the next navigation link
 		else {
 			$('.navigation li[data-slide="' + (dataslide - 1) + '"]').addClass('active').next().removeClass('active');
 		}
@@ -61,47 +100,42 @@ jQuery(document).ready(function ($) {
 		}
 	})
 	
-	//waypoints doesnt detect the first slide when user scrolls back up to the top so we add this little bit of code, that removes the class
-	//from navigation link slide 2 and adds it to navigation link slide 1.
 	mywindow.scroll(function () {
-		if (mywindow.scrollTop() == 0) {
+		if (mywindow.scrollTop() === 0) {
 			$('.navigation li[data-slide="0"]').addClass('active');
 			$('.navigation li[data-slide="1"]').removeClass('active');
 		}
 		if ($('.navigation li[data-slide="1"],.navigation li[data-slide="2"],.navigation li[data-slide="3"],.navigation li[data-slide="4"],.navigation li[data-slide="5"],.navigation li[data-slide="6"],.navigation li[data-slide="7"]').hasClass('active')) {
-			$('.navigation li').removeClass('light-menu').addClass('dark-menu');
+			$('.navigation li, .header, .footer').removeClass('light-menu').addClass('dark-menu');
 		}  else if ($('.navigation li[data-slide="0"]').hasClass('active')) {
-			$('.navigation li').removeClass('dark-menu').addClass('light-menu');
+			$('.navigation li, .header, .footer').removeClass('dark-menu').addClass('light-menu');
 		}
 	});
 	
-	//Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
-	//easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
 	function goToByScroll(dataslide) {
 		htmlbody.animate({
 			scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
 		}, 2000, 'easeInOutQuint');
 	}
 	
-	//When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
 	links.click(function (e) {
 		e.preventDefault();
-		dataslide = $(this).attr('data-slide');
+		let dataslide = $(this).attr('data-slide');
 		goToByScroll(dataslide);
 	});
 	
-	//When the user clicks on the button, get the get the data-slide attribute value of the button and pass that variable to the goToByScroll function
 	button.click(function (e) {
 		e.preventDefault();
-		dataslide = $(this).attr('data-slide');
+		let dataslide = $(this).attr('data-slide');
 		goToByScroll(dataslide);
 	});
 	
 });
 
-var isScroll;
-var lastScroll;
-var scrollDir;
+// Нахождение напровления прокрутки мыши
+let isScroll;
+let lastScroll;
+let scrollDir;
 
 function checkScrolling () {
 	let headerHeight = 100
